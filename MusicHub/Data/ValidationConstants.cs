9c60999590a0796ad645dc124c8d8a50a0e83838
ThinkPad_P53s <ThinkPad_P53s@DESKTOP-3PGJHFO>
2023-03-26 20:24:40 +0300

    Add project files.

---
+++ MusicHub/Data/ValidationConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicHub.Data;
+
+public static class ValidationConstants
+{
+	public const int  VlidationLengthSongName = 20;
+	public const int ValidationLengthAlbumName = 40;
+	public const int PerformerFirstNameMaxLength = 20;
+	public const int PerformerLastNameMaxLength = 20;
+	public const int ProducerNameMaxLength = 20;
+	public const int WriterNameMaxLength = 30;
+}
